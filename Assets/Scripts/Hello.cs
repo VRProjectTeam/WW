1b1242f00e7105727ace3531e918c5e8290e19dc
Ziv <1805358794@qq.com>
2018-05-17 22:18:08 +0800

    我说刘静浩，以成功

--- Assets/Scripts/Hello.cs
+++ Assets/Scripts/Hello.cs
@@ -8,7 +8,7 @@ public class Hello : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        Debug.Log("你好，我是刘静浩！11");
+        Debug.Log("你好，我是刘静浩");
 	}
 	
 	// Update is called once per frame
