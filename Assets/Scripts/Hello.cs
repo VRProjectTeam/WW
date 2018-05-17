906e966b32d6dab23ca0ae7b90ce05d383eec441
Augn <wang1183478375@outlook.com>
2018-05-17 13:46:05 +0800

    Creat Project

---
+++ Assets/Scripts/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("你好，项目期开始了，请各位加油！");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
