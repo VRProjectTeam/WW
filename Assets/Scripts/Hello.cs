4900884a2b8b808557d5d1fbad6071af251d946c
熊腾 <@MSIXT>
2018-05-18 08:26:21 +0800

    我是熊腾，以成功

--- Assets/Scripts/Hello.cs
+++ Assets/Scripts/Hello.cs
@@ -9,7 +9,8 @@ public class Hello : MonoBehaviour
 	void Start ()
     {
         Debug.Log("你好，项目期开始了，请各位加油！");
-	}
+        Debug.Log("我是熊腾，以成功");
+    }
 	
 	// Update is called once per frame
 	void Update ()
