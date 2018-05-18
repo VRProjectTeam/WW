78bfad44621f57be2255d49d90022948a643de3a
1637961356 <1637961356@qq.com>
2018-05-18 18:02:08 +0800

    lll

--- Assets/Scripts/Hello.cs
+++ Assets/Scripts/Hello.cs
@@ -11,6 +11,7 @@ public class Hello : MonoBehaviour
         Debug.Log("你好，项目期开始了，请各位加油！");
         Debug.Log("我是熊腾，以成功");
         Debug.Log("我是杨希钢，已成功");
+        Debug.Log("我是李勇，已成功");
     }
 	
 	// Update is called once per frame
