f194c73fc38be25b94a79039a82a4a72baf43250
鑫源 李 <3371390581@qq.com>
2018-05-18 14:33:53 +0800

    我是李鑫源，已成功！

--- Assets/Scripts/Hello.cs
+++ Assets/Scripts/Hello.cs
@@ -9,7 +9,8 @@ public class Hello : MonoBehaviour
 	void Start ()
     {
         Debug.Log("你好，项目期开始了，请各位加油！");
-	}
+        Debug.Log("我是李鑫源，已成功！");
+    }
 	
 	// Update is called once per frame
 	void Update ()
